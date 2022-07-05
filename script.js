7f9d20ee57af1e3c6ae83281a183cb3a5bd4f237
Philippe Covington <philippehcov@gmail.com>
2022-07-05 09:23:11 -0400

    Hmmm

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const API_KEY = ''
+const API_KEY = 'api_key=a37ffc7d0f58d1417019ffc8f414d002';
 const DOMAIN = 'https://api.themoviedb.org/3'
-const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/original'
+const IMAGE_BASE_PATH = DOMAIN + '/discover/movie?sort_by=popularity.desc'
+
+const searchButton = document.getElementById('search')
+const movieInput = document.querySelector('.search')
+const movieList = document.querySelector('.movie-list')
+
+searchButton.addEventListener('click', async () => {
+  let movie = movieInput.value;
+  let response = await axios.get(DOMAIN+'search/movie?query='+movie+'api_key='+API_KEY)
+  let searchedMovie = response.data.message;
+  movieList.innerHTML = 
+  `
+    <img src=${response}>
+    <h3>${movie}</h3>
+  `
+})
+
